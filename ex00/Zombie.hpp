bd1d0ede1c747ec37fc05a8b645e02aa74eae2cf
Rehxco <salome.brchrd@gmail.com>
2026-02-27 16:19:55 +0100

    presque rien

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbrochar <sbrochar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/26 17:34:12 by sbrochar          #+#    #+#             */
+/*   Updated: 2026/02/27 15:57:33 by sbrochar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <cstdlib>
+# include <iomanip>
+# include <iostream>
+
+class Zombie
+{
+  private:
+	std::string name;
+
+  public:
+	Zombie(std::string name);
+	~Zombie();
+	void announce();
+};
+Zombie	*newZombie(std::string name);
+
+#endif
